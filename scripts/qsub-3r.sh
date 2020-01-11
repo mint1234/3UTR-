f3ff96795e4174d704359da9f2595e28a849c57a
MinTu <mint1234@users.noreply.github.com>
2020-01-10 20:14:16 -0500

    Update qsub-3r.sh

--- scripts/qsub-3r.sh
+++ scripts/qsub-3r.sh
@@ -2,14 +2,11 @@
 
 ###---Function---sumbit 3r.sh---###
 
-# add all sample names: one conditions for example.
-SNAME=(Endosperm_20DAP)
+# add all sample names: two conditions for example.
+SNAME=(Endosperm_16DAP Endosperm_20DAP)
 
-# three conditions will be :
-# SNAME=(Endosperm_16DAP Endosperm_20DAP Endosperm_24DAP)
-
-## cycle all of conditions:  i corresponds to numbers of condition. For example "i<10" for 10 conditions
-for ((i=0;i<1;i++))
+## cycle all of conditions:  i corresponds to numbers of condition. 
+for ((i=0;i<2;i++))
 do
    qsub -v SN=${SNAME[$i]} /ingens/home/mintu/scripts/3r.sh
 done
